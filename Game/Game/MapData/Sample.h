8fb05b48cff015c7f7166de330fabb94be7afbe8
Matsuoka <t>
2016-11-12 17:52:49 +0900

    Change Sample.h

--- Game/Game/MapData/Sample.h
+++ Game/Game/MapData/Sample.h
@@ -1,12 +1,18 @@
-﻿//Building_a
+﻿//map
+{
+	"map",
+	CVector3(0.00f, -5.50f, 0.00f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
+},
+//Building_a
 {
 	"Building_a",
-	CVector3(-3.09f, -1.26f, -7.54f),             //平行移動
-	CQuaternion(-0.71f, -0.01f, -0.01f, 0.71f ),  //回転
+	CVector3(0.00f, -1.30f, -6.40f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
 },
-//Building_b
+//Building_a
 {
-	"Building_b",
-	CVector3(-15.35f, 0.00f, -2.38f),             //平行移動
-	CQuaternion(-0.62f, 0.35f, 0.35f, 0.62f ),  //回転
+	"Building_a",
+	CVector3(15.29f, -2.50f, -6.40f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ),  //回転
 },
