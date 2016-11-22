a2d303f32766c4789bf40865a3a98b823b9293e3
村上恒平 <mikan.zuki319@gmail.com>
2016-11-22 16:20:19 +0900

    add Enemy.cpp

--- Game/Game/Enemy.cpp
+++ Game/Game/Enemy.cpp
@@ -6,7 +6,7 @@ Enemy::Enemy()
 {
 	m_position = {1.0f,0.0f,0.0f};
 	m_rotation = CQuaternion::Identity;
-	m_scale = { 0.8f,0.8f,0.8f };
+	m_scale = { 0.6f,0.6f,0.6f };
 
 }
 
@@ -27,11 +27,18 @@ void Enemy::Start()
 
 void Enemy::Update()
 {
+	Move();
 	m_skinModel.Update(m_position, m_rotation,m_scale);//ワールド行列の更新。
 }
 
 void Enemy::Move()
 {
+	CVector3 move = m_characterController.GetMoveSpeed();
+	//決定した移動速度をキャラクタコントローラーに設定。
+	m_characterController.SetMoveSpeed(move);
+	//キャラクターコントローラーを実行。
+	m_characterController.Execute();
+	m_position = m_characterController.GetPosition();
 }
 
 void Enemy::Render(CRenderContext& renderContext)
