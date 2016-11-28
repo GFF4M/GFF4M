26b44f0a370f23625b3e776cf144107cc540161e
Matsuoka <t>
2016-11-28 11:12:53 +0900

    add main ID and Pass

--- Game/Game/ModelHandle.h
+++ Game/Game/ModelHandle.h
@@ -6,6 +6,14 @@ public:
 	ModelHandle();
 	~ModelHandle();
 
+	CSkinModelDataHandle& GetModel_Player()
+	{
+		return m_model_player;
+	};
+
 private:
+	CSkinModelDataHandle	m_model_player;				//スキンモデルデータハンドル。
 };
+
+extern ModelHandle* g_modelhandle;
 
