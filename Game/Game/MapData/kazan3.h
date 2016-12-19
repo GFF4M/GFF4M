41ef842a651054050dbcd55fb63e2a5e87f768e1
村上恒平 <mikan.zuki319@gmail.com>
2016-12-19 12:22:11 +0900

    add mapname

---
+++ Game/Game/MapData/kazan3.h
@@ -0,0 +1,63 @@
+﻿//Building_a
+{
+	"Building_a",
+	CVector3(3.80f, 12.40f, -14.60f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
+	CVector3(1.00f, 1.00f, 1.00f),             //スケール
+},
+//Building_a
+{
+	"Building_a",
+	CVector3(-14.50f, 12.60f, -17.70f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
+	CVector3(1.00f, 1.00f, 1.00f),             //スケール
+},
+//saku
+{
+	"saku",
+	CVector3(9.16f, 13.70f, -9.21f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ), //回転
+	CVector3(0.06f, 0.05f, 0.06f),             //スケール
+},
+//saku
+{
+	"saku",
+	CVector3(26.00f, 13.70f, -9.21f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ), //回転
+	CVector3(0.06f, 0.05f, 0.06f),             //スケール
+},
+//saku
+{
+	"saku",
+	CVector3(-9.09f, 13.78f, -9.21f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ), //回転
+	CVector3(0.06f, 0.05f, 0.06f),             //スケール
+},
+//saku
+{
+	"saku",
+	CVector3(-9.09f, 20.79f, -9.21f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ), //回転
+	CVector3(0.06f, 0.05f, 0.06f),             //スケール
+},
+//saku
+{
+	"saku",
+	CVector3(-9.07f, 18.61f, -9.21f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ), //回転
+	CVector3(0.06f, 0.05f, 0.06f),             //スケール
+},
+//saku
+{
+	"saku",
+	CVector3(-9.09f, 16.07f, -9.21f),             //平行移動
+	CQuaternion(0.00f, 0.00f, 0.00f, 1.00f ), //回転
+	CVector3(0.06f, 0.05f, 0.06f),             //スケール
+},
+//kazan2
+{
+	"kazan2",
+	CVector3(8.70f, 33.00f, -31.40f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
+	CVector3(1.38f, 1.38f, 1.38f),             //スケール
+},
---
+++ Game/Game/MapData/sougen2.h
@@ -0,0 +1,21 @@
+﻿//Building_a
+{
+	"Building_a",
+	CVector3(3.80f, 14.42f, -14.60f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
+	CVector3(1.00f, 1.00f, 1.00f),             //スケール
+},
+//Building_a
+{
+	"Building_a",
+	CVector3(22.50f, 14.57f, -17.70f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
+	CVector3(1.00f, 1.00f, 1.00f),             //スケール
+},
+//sougen
+{
+	"sougen",
+	CVector3(-9.50f, 24.30f, -38.40f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
+	CVector3(1.66f, 2.11f, 1.00f),             //スケール
+},
