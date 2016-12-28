a87241d7041c974caf34077b90f3c138e18a52c4
村上恒平 <mikan.zuki319@gmail.com>
2016-12-29 01:43:31 +0900

    add sougen3.h saku

--- Game/Game/MapData/sougen2.h
+++ Game/Game/MapData/sougen2.h
@@ -1,28 +1,63 @@
-﻿//Flag
+﻿//Building_a
+{
+	"Building_a",
+	CVector3(0.33f, 53.44f, -14.73f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
+	CVector3(1.00f, 1.00f, 1.00f),             //スケール
+},
+//Building_a
+{
+	"Building_a",
+	CVector3(-14.50f, 53.70f, -17.70f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
+	CVector3(1.00f, 1.00f, 1.00f),             //スケール
+},
+//sakusaku
+{
+	"sakusaku",
+	CVector3(16.22f, 53.16f, 6.00f),             //平行移動
+	CQuaternion(-0.49f, 0.50f, 0.52f, 0.48f ), //回転
+	CVector3(0.05f, 0.05f, 0.10f),             //スケール
+},
+//sakusaku
+{
+	"sakusaku",
+	CVector3(-33.00f, 52.20f, 16.80f),             //平行移動
+	CQuaternion(-0.51f, -0.48f, -0.49f, 0.51f ), //回転
+	CVector3(0.05f, 0.05f, 0.10f),             //スケール
+},
+//Flag
 {
 	"Flag",
-	CVector3(80.60f, -10.60f, 28.50f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
-	CVector3(0.40f, 1.00f, 1.00f),             //スケール
-},
-//Building_a
-{
-	"Building_a",
-	CVector3(3.80f, 1.60f, -14.60f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
-	CVector3(1.00f, 1.00f, 1.00f),             //スケール
-},
-//Building_a
-{
-	"Building_a",
-	CVector3(-14.50f, 1.70f, -17.70f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
-	CVector3(1.00f, 1.00f, 1.00f),             //スケール
+	CVector3(2.60f, 51.95f, 149.60f),             //平行移動
+	CQuaternion(-0.64f, -0.31f, -0.31f, 0.64f ), //回転
+	CVector3(0.08f, 0.21f, 0.21f),             //スケール
 },
 //sougen
 {
 	"sougen",
-	CVector3(16.00f, 9.70f, -13.80f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
+	CVector3(-10.60f, 63.00f, 61.00f),             //平行移動
+	CQuaternion(-0.64f, -0.31f, -0.31f, 0.64f ), //回転
 	CVector3(1.66f, 2.11f, 1.00f),             //スケール
+},
+//sakusaku
+{
+	"sakusaku",
+	CVector3(-29.10f, 52.20f, 87.30f),             //平行移動
+	CQuaternion(-0.51f, -0.48f, -0.49f, 0.51f ), //回転
+	CVector3(0.05f, 0.05f, 0.10f),             //スケール
+},
+//sakusaku
+{
+	"sakusaku",
+	CVector3(19.72f, 52.98f, 76.50f),             //平行移動
+	CQuaternion(-0.49f, 0.50f, 0.52f, 0.48f ), //回転
+	CVector3(0.05f, 0.05f, 0.10f),             //スケール
+},
+//sakusaku
+{
+	"sakusaku",
+	CVector3(-26.40f, 52.21f, 144.60f),             //平行移動
+	CQuaternion(-0.51f, -0.48f, -0.49f, 0.51f ), //回転
+	CVector3(0.03f, 0.05f, 0.10f),             //スケール
 },
--- Game/Game/MapData/sougen3.h
+++ Game/Game/MapData/sougen3.h
@@ -1,28 +1,77 @@
-﻿//Flag
+﻿//saku
 {
-	"Flag",
-	CVector3(-0.10f, -10.60f, 28.50f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
-	CVector3(0.40f, 1.00f, 1.00f),             //スケール
+	"saku",
+	CVector3(4.10f, 53.78f, -34.77f),             //平行移動
+	CQuaternion(-0.52f, -0.48f, -0.48f, 0.52f ), //回転
+	CVector3(0.10f, 0.01f, 0.14f),             //スケール
+},
+//saku
+{
+	"saku",
+	CVector3(3.87f, 53.55f, -39.98f),             //平行移動
+	CQuaternion(-0.52f, -0.48f, -0.48f, 0.52f ), //回転
+	CVector3(0.10f, 0.01f, 0.14f),             //スケール
+},
+//saku
+{
+	"saku",
+	CVector3(3.58f, 53.57f, -44.97f),             //平行移動
+	CQuaternion(-0.52f, -0.48f, -0.48f, 0.52f ), //回転
+	CVector3(0.09f, 0.01f, 0.14f),             //スケール
 },
 //Building_a
 {
 	"Building_a",
-	CVector3(3.80f, 1.60f, -14.60f),             //平行移動
+	CVector3(0.33f, 53.44f, -14.73f),             //平行移動
 	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
 	CVector3(1.00f, 1.00f, 1.00f),             //スケール
 },
 //Building_a
 {
 	"Building_a",
-	CVector3(-14.50f, 1.70f, -17.70f),             //平行移動
+	CVector3(-14.50f, 53.70f, -17.70f),             //平行移動
 	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
 	CVector3(1.00f, 1.00f, 1.00f),             //スケール
 },
 //sougen2
 {
 	"sougen2",
-	CVector3(-49.56f, 3.60f, -5.78f),             //平行移動
+	CVector3(-49.56f, 57.40f, -5.78f),             //平行移動
 	CQuaternion(0.01f, 0.71f, 0.71f, -0.01f ), //回転
 	CVector3(1.00f, 1.00f, 1.00f),             //スケール
+},
+//sakusaku
+{
+	"sakusaku",
+	CVector3(4.17f, 53.24f, -2.31f),             //平行移動
+	CQuaternion(-0.51f, 0.49f, 0.50f, 0.49f ), //回転
+	CVector3(0.05f, 0.05f, 0.10f),             //スケール
+},
+//sakusaku
+{
+	"sakusaku",
+	CVector3(-19.97f, 52.73f, -50.70f),             //平行移動
+	CQuaternion(-0.71f, 0.01f, 0.01f, 0.70f ), //回転
+	CVector3(0.05f, 0.05f, 0.10f),             //スケール
+},
+//saku
+{
+	"saku",
+	CVector3(4.10f, 53.78f, -29.24f),             //平行移動
+	CQuaternion(-0.52f, -0.48f, -0.48f, 0.52f ), //回転
+	CVector3(0.10f, 0.01f, 0.14f),             //スケール
+},
+//Flag
+{
+	"Flag",
+	CVector3(-20.80f, 52.70f, 39.60f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
+	CVector3(0.08f, 0.21f, 0.21f),             //スケール
+},
+//saku
+{
+	"saku",
+	CVector3(3.58f, 53.57f, -49.19f),             //平行移動
+	CQuaternion(-0.52f, -0.48f, -0.48f, 0.52f ), //回転
+	CVector3(0.09f, 0.01f, 0.14f),             //スケール
 },
