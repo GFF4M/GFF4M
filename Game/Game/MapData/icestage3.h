34309a09606a1531d868fc7e636535a473e63301
村上恒平 <mikan.zuki319@gmail.com>
2016-12-29 10:33:21 +0900

    add x

--- Game/Game/MapData/icestage3.h
+++ Game/Game/MapData/icestage3.h
@@ -1,42 +1,35 @@
-﻿//Flag
+﻿//Building_a
+{
+	"Building_a",
+	CVector3(0.33f, 53.44f, -14.73f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
+	CVector3(1.00f, 1.00f, 1.00f),             //スケール
+},
+//Building_a
+{
+	"Building_a",
+	CVector3(-14.50f, 53.70f, -17.70f),             //平行移動
+	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
+	CVector3(1.00f, 1.00f, 1.00f),             //スケール
+},
+//Flag
 {
 	"Flag",
-	CVector3(61.70f, -10.60f, -49.30f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
-	CVector3(0.40f, 1.00f, 1.00f),             //スケール
-},
-//Building_a
-{
-	"Building_a",
-	CVector3(3.80f, 1.60f, -14.60f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
-	CVector3(1.00f, 1.00f, 1.00f),             //スケール
-},
-//Building_a
-{
-	"Building_a",
-	CVector3(-14.50f, 1.70f, -17.70f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
-	CVector3(1.00f, 1.00f, 1.00f),             //スケール
-},
-//turara
-{
-	"turara",
-	CVector3(-31.70f, -0.30f, 14.30f),             //平行移動
-	CQuaternion(-0.71f, 0.00f, 0.00f, 0.71f ), //回転
-	CVector3(0.88f, 0.88f, 0.88f),             //スケール
-},
-//turara2
-{
-	"turara2",
-	CVector3(-3.50f, 5.70f, -1.40f),             //平行移動
-	CQuaternion(-0.52f, 0.47f, 0.47f, 0.52f ), //回転
-	CVector3(0.40f, 0.40f, 0.40f),             //スケール
+	CVector3(-26.00f, 49.00f, 121.70f),             //平行移動
+	CQuaternion(-0.13f, -0.70f, -0.70f, 0.13f ), //回転
+	CVector3(0.08f, 0.21f, 0.21f),             //スケール
 },
 //icestage3
 {
 	"icestage3",
-	CVector3(146.48f, 21.93f, -19.50f),             //平行移動
-	CQuaternion(-0.51f, -0.48f, 0.47f, 0.54f ), //回転
-	CVector3(0.75f, 1.00f, 1.72f),             //スケール
+	CVector3(-73.70f, 62.00f, 192.50f),             //平行移動
+	CQuaternion(-0.69f, -0.70f, -0.16f, -0.06f ), //回転
+	CVector3(0.73f, 0.74f, 1.78f),             //スケール
+},
+//turara3
+{
+	"turara3",
+	CVector3(-51.90f, 35.30f, 65.40f),             //平行移動
+	CQuaternion(-0.68f, -0.19f, -0.19f, 0.68f ), //回転
+	CVector3(1.00f, 1.00f, 0.85f),             //スケール
 },
